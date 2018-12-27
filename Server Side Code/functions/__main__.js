05523eb3e99779a266eacb23b160a3e6f9d21f06
Sagar Patel <s.72427patel@gmail.com>
2018-12-27 13:05:37 -0500

    Server Side

---
+++ Server Side Code/functions/__main__.js
@@ -0,0 +1,64 @@
+/**
+* Access for Google Maps API
+* @param {string} sender
+* @param {string} start
+* @param {string} end
+* @param {string} method
+* @returns {string}
+*/
+
+const lib = require('lib')({token: process.env.token});
+var rp = require('request');
+var reqProm = require('request-promise');
+const Promise = require('bluebird');
+const sms = lib.utils.sms['@1.0.9'];
+
+module.exports = async (sender = "", start = "", end = "", method = "", context) => {
+
+    //process start
+    start = start.replace(/,/g, '+')
+    end = end.replace(/,/g, '+')
+
+    //Enter Google Maps Api in env.json
+    let apikey = process.env.googleapikey
+
+    let googlecall = await 'https://maps.googleapis.com/maps/api/directions/json?key=' + apikey +
+                        '&mode=' + method +
+                        '&origin=' + start +
+                        '&destination=' + end
+    googlecall = googlecall.replace(/ /g,"%20")
+
+
+    let res = await reqProm({               //wait for GET request
+        url: googlecall
+      }).then(function(directions) {        //push response to next
+        return directions
+      }).then(d =>{                         //where we do stuff with the body
+        d = JSON.parse(d)
+
+        let totalD = d.routes[0].legs[0].distance.text
+        let totalT = d.routes[0].legs[0].duration.text
+
+        let response = {}
+        response['Distance'] = totalD
+        response['Duration'] = totalT
+
+        response['Steps'] = []
+        for (i = 0; i < d.routes[0].legs[0].steps.length; i++){
+          let step = String(i+1) + ") " + String(d.routes[0].legs[0].steps[i].html_instructions)
+          step = step.replace(/<\/?[^>]+(>|$)/g, "");
+          response.Steps.push(step)
+        }
+        return response;
+      })
+      
+      
+      
+    //Send Text
+    let result = await sms({
+      to: sender, // (required)
+      body: ((JSON.stringify(res,null,2)).slice(1, -1)).replace(/\"[]/g, "") // (required)
+    });
+    
+    return process.env.token
+};
